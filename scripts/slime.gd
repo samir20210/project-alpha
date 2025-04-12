b94654ee7951bf71fad15c6fba50e436afea6983
SamDam <samirchik.21022000@gmail.com>
2025-04-12 14:23:52 +0300

    first commit

---
+++ scripts/slime.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+const speed = 60
+
+var direction = 1
+
+@onready var raycast_right: RayCast2D = $raycastRight
+@onready var raycast_left: RayCast2D = $raycastLeft
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _process(delta):
+	if raycast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+	if raycast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+	position.x += speed * delta * direction
